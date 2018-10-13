e6aad81cfacbfe194cb42b1b459d8aa9b92235bd
sinan nar <sinan.nar@gmail.com>
2018-10-13 13:26:02 +1300

    entities added

---
+++ aspnet-core/src/NorthShore.Restaurant.Core/Restaurant/Menu.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace NorthShore.Restaurant.Restaurant
+{
+    public class Menu: Entity<long>
+    {
+        public string Name { get; set; }
+        public decimal DiscountRate { get; set; }
+        public decimal TotalCalorie{get;set;}
+        public decimal TotalPrice{get;set;}
+        public decimal DiscountedPrice{get;set;}
+        public IEnumerable<FoodMenuMapping> FoodMappings { get; set; }
+    }
+}
