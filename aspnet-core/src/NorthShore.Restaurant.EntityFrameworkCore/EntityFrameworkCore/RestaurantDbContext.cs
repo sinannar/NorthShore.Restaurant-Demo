1844b976d2124249a2e94c21ad56df7f83833237
sinan nar <sinan.nar@gmail.com>
2018-10-13 13:27:17 +1300

    db context has been updated for entities

--- aspnet-core/src/NorthShore.Restaurant.EntityFrameworkCore/EntityFrameworkCore/RestaurantDbContext.cs
+++ aspnet-core/src/NorthShore.Restaurant.EntityFrameworkCore/EntityFrameworkCore/RestaurantDbContext.cs
@@ -9,10 +9,33 @@ namespace NorthShore.Restaurant.EntityFrameworkCore
     public class RestaurantDbContext : AbpZeroDbContext<Tenant, Role, User, RestaurantDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public virtual DbSet<Menu> Menus { get; set; }
+        public virtual DbSet<Food> Foods { get; set; }
+        public virtual DbSet<FoodMenuMapping> FoodMenuMappings { get; set; }
+
         public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<FoodMenuMapping>(fmp => {
+
+                fmp.HasOne(m => m.Food)
+                    .WithMany(f => f.MenuMappings)
+                    .HasPrincipalKey(f => f.Id)
+                    .HasForeignKey(m => m.FoodId)
+                    .HasConstraintName("FK_FoodMenuMapping_Food");
+
+                fmp.HasOne(m => m.Menu)
+                    .WithMany(m => m.FoodMappings)
+                    .HasPrincipalKey(m => m.Id)
+                    .HasForeignKey(m => m.MenuId)
+                    .HasConstraintName("FK_FoodMenuMapping_Mood");
+            });
         }
     }
 }
