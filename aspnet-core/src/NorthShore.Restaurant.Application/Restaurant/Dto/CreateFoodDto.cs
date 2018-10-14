4cfb0db66668459ebfec842ad0363afb14c96741
sinan nar <sinan.nar@gmail.com>
2018-10-14 16:16:35 +1300

    create food api implemented

---
+++ aspnet-core/src/NorthShore.Restaurant.Application/Restaurant/Dto/CreateFoodDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthShore.Restaurant.Restaurant.Dto
+{
+    public class CreateFoodDto
+    {
+        public int Calorie { get; set; }
+        public string Name { get; set; }
+    }
+}
